4cb35b7422677bdd5dbce84f662d877ae65b26fe
Cole Taylor <coletaylor017@gmail.com>
2018-12-05 17:33:41 +0000

    add process.env.IP to listen statement

--- app.js
+++ app.js
@@ -221,6 +221,6 @@ function isLoggedIn(req, res, next) {
     res.redirect("/login");
 }
 
-app.listen(process.env.PORT, function() {
+app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Server is now running");
 });
