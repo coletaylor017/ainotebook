d9f8a79493cd527265f0ccb712780aeb6fe08175
Cole Taylor <coletaylor017@gmail.com>
2019-01-10 06:11:45 +0000

    add word count on account page

--- TODO.md
+++ TODO.md
@@ -6,7 +6,6 @@ General
         -double check that they want to delete before deleting
     -Sign up validation
     -Flash error messages for invalid login, etc.
-    -User page word count
     -Tour for new users
     -Metadata. Create a new collection called "metadata" and have each document be an object with a data name, value, date created, owner (referencing "users"), and entry id (referencing "entries"). When metadata is analyzed, find() bits of data based on their date, name, and/or entry and ALWAYS their user. Then the returned array of data can have its information pulled out and displayed. 
         -Consider displaying metadata semi-graphically on the entry "show" page.
@@ -29,3 +28,4 @@ Done!
     -Should be easy: Upon loading log in page, if user is logged in, redirect to /entries
     -Tags side bar, like a blog. Make a "tags" collection with each document being a particular tag with a reference to the user it belongs to and an array of the entries that reference it. Every time an entry is created or updated, the tag line is parsed and the entry is added to the appropriate tag objects in the "tags" collection is it's not already there.
     -Issue: users still reference post IDs after the posts are deleted
+    -User page word count
--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var methodOverride        = require("method-override"),
     express               = require("express"),
     request               = require("request"),
     moment                = require("moment"),
-    ridict                = require('ridict'),
+    ridict                = require("ridict"),
     Entry                 = require("./models/entry"),
     User                  = require("./models/user"),
     Tag                   = require("./models/tag"),
@@ -415,12 +415,18 @@ app.post("/login", passport.authenticate("local", {
 });
 
 app.get("/account", isLoggedIn, function(req, res) {
-    Entry.find({"author.id": req.user._id}, function(err, entries) {
+    Entry.find({"author.id": req.user._id}, {body: 1, _id: 0}, function(err, entries) {
         if (err) {
             console.log(err);
         }
+        var wordCount = 0;
+        entries.forEach(function(entry) {
+            var l = entry.body.split(" ");
+            wordCount += l.length;
+        });
         var entryCount = entries.length;
-        res.render("account", {entryCount: entryCount});
+        res.render("account", {entryCount: entryCount, wordCount: wordCount});
+        // Question: Is it better to do calculation on the server side or pass the entire entry body to the client and do the calculation there?
     });
 });
 
