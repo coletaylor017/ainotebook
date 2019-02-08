18ba3302572903713f4c000427dacaa768220317
Cole Taylor <coletaylor017@gmail.com>
2019-02-08 05:33:57 +0000

    Add quote changing algorithm

--- middleware/index.js
+++ middleware/index.js
@@ -1,4 +1,5 @@
 var Global    = require("../models/global"),
+    Quote     = require("../models/quote"),
     mongoose  = require("mongoose");
 
 var middlewareObj = {
@@ -18,12 +19,43 @@ var middlewareObj = {
         res.redirect("/home");
     },
     updateQuote: function(req, res, next) {
-        Global.find({}, function(err, global) { // There will only ever be one
+        Quote.find({}, {index: 1, _id: 0}, function(err, quotes) {
             if (err) {
                 console.log(err);
             } else {
-                console.log("Global to be updated", global);
-                return next();
+                var min = quotes[0].index;
+                for (var i = 1; i < quotes.length; i++) {
+                    if (quotes[i].index < min) {
+                        min = quotes[i].index;
+                    }
+                }
+                console.log("Indicies: ", quotes);
+                console.log("min: ", min)
+                
+                Quote.find({index: min}, function(err, candidates) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        var luckyWinner = Math.floor(Math.random()*candidates.length);
+                        var winnerId = candidates[luckyWinner]._id;
+                        console.log(luckyWinner, winnerId);
+                        Quote.findById(winnerId, function(err, quote) {
+                            if (err) {
+                                console.log(err);
+                            } else {
+                                quote.index++;
+                                quote.save();
+                                Global.update({}, { currentQuote: quote }, function(err, global) { // There should only ever be one
+                                    if (err) {
+                                        console.log(err);
+                                    } else {
+                                        return next();
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
             }
         });
     }
